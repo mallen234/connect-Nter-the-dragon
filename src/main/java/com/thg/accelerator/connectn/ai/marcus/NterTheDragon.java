f0e0ca73ef8bd70b7d16d0a9095d085ebfbfc6d9
Marcus Allen <marcus.allen84@thehutgroup.com>
2024-01-22 16:20:39 +0000

    initial AI

--- src/main/java/com/thg/accelerator/connectn/ai/marcus/NterTheDragon.java
+++ src/main/java/com/thg/accelerator/connectn/ai/marcus/NterTheDragon.java
@@ -1,20 +1,27 @@
-package com.thg.accelerator23.connectn.ai.yourteam;
+package com.thg.accelerator.connectn.ai.marcus;
 
 import com.thehutgroup.accelerator.connectn.player.Board;
 import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.GameConfig;
 import com.thehutgroup.accelerator.connectn.player.Player;
 
+import java.util.Random;
 
-public class NameAi extends Player {
-  public NameAi(Counter counter) {
+
+public class NterTheDragon extends Player {
+  public NterTheDragon(Counter counter) {
     //TODO: fill in your name here
-    super(counter, NameAi.class.getName());
+    super(counter, NterTheDragon.class.getName());
   }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
+
+    Random rand = new Random();
+
+    GameConfig currboard = board.getConfig();
     return 4;
   }
 }
